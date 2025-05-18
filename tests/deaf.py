6e8f7970fbfa7d68c4de3f87aa6654bbd0d5719f
Daisy Universe <daisy@universe.dog>
2025-05-18 14:38:49 -0500

    Update deaf.py

--- tests/deaf.py
+++ tests/deaf.py
@@ -1,9 +1,10 @@
-# This is a simple pygame script exploring animated sprites
+#This is a simple pygame script exploring animated sprites
 
 # Import libs
 import pygame
 import sys
 import os
+import math
 
 # Initialize Pygame
 pygame.init()
@@ -11,8 +12,8 @@ pygame.init()
 # Set up the display
 WIDTH, HEIGHT = 480, 480
 spritesize = 570
-hoffset = 22
-voffset = 5
+hoffset = 20
+voffset = -5
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pygame Animated Sprites Example")
 
@@ -23,9 +24,19 @@ sprite_folder = os.path.join(project_dir, "img/deaf_bmp")
 frames = len(next(os.walk(sprite_folder))[2])
 print(frames)
 
+clock = pygame.time.Clock()
+frame = 0
+frame_rate = 30
+
+BG = (0, 0, 0)
+
 # Load images for animation
 sprite_images = []
 for i in range(000, frames):
+    BG = (min(i*2, 255), min(i*2, 255), min(i*2, 255))
+    screen.fill(BG)
+    pygame.display.flip()
+
     formatted_i = "{:03d}".format(i) # hack to get leading zeros in i (cfr. img names)
     print(f"\r[ LOADING FRAME {formatted_i}/{frames} ]", end='')
     image_path = os.path.join(sprite_folder, f"frame_{formatted_i}.bmp")
@@ -33,21 +44,11 @@ for i in range(000, frames):
     image = pygame.transform.scale(image, (spritesize, spritesize))
     sprite_images.append(image)
 
+    clock.tick()
+
 # Get the dimensions of the sprite image
 sprite_width = spritesize
 sprite_height = spritesize
-
-# Define Colors
-BROWN = (101, 32, 32)
-
-# Set up clock
-clock = pygame.time.Clock()
-
-# Game loop
-frame = 0
-
-# set frame rate (FPS)
-frame_rate = 30
 
 # center sprite
 sprite_x, sprite_y = (WIDTH//2-sprite_width//2+hoffset),(HEIGHT//2-sprite_height//2+voffset)
@@ -60,7 +61,7 @@ while running:
             running = False
 
     # Clear the screen
-    screen.fill(BROWN)
+    screen.fill(BG)
 
     # Draw the current frame of the animation
     screen.blit(sprite_images[frame], (sprite_x, sprite_y))
